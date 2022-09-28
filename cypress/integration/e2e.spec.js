b86cca40e692dd385c7e21e63fed890a7a582eca
Fernando Bergamini <fernandobr.89@hotmail.com>
2022-09-28 12:10:52 -0300

    alterações

--- cypress/integration/e2e.spec.js
+++ cypress/integration/e2e.spec.js
@@ -1,4 +1,4 @@
-/// <reference types="cypress" />
+<reference types="cypress"/>
 
 context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
     /*  Como cliente 
@@ -9,13 +9,28 @@ context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
         Preenchendo todas opções no checkout
         E validando minha compra ao final */
 
-    beforeEach(() => {
-        cy.visit('/')
-    });
+        beforeEach(() => {
+            cy.visit ('minha-conta')
+            cy.login('aluno_ebac@teste.com','teste@teste.com')
+            cy.get('.logo-in-theme > .logo > a > .logo-img').click()
+                
+        });
 
-    it('Deve fazer um pedido na loja Ebac Shop de ponta a ponta', () => {
-        //TODO 
+    it.only('Deve fazer um pedido na loja Ebac Shop de ponta a ponta', () => {
+    var quantidade= 4
+      
+    cy.get('[class="product-block grid"]')
+        .contains ('Ingrid Running Jacket').click()
+        cy.get('.button-variable-item-S').click()
+        cy.get('.button-variable-item-Orange').click()
+        cy.get('.input-text').clear().type(quantidade)
+        cy.get('.single_add_to_cart_button').click()
+      
+        cy.get('.dropdown-toggle > .text-skin > .icon-basket').click()
+        cy.get('#cart > .dropdown-menu > .widget_shopping_cart_content > .mini_cart_content > .mini_cart_inner > .mcart-border > .buttons > .view-cart').click()
+        cy.get('.checkout-button').click()
+        cy.get('#terms').click()
+        cy.get('.place-order').click()              
     });
-
 
 })
